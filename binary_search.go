d79efad9974ae1793bab4f1c178f086fa2dcf7a4
Igor Pavlichenko <igor.o.pavlichenko@gmail.com>
2024-05-14 00:57:21 +0100

    Implement binary search

---
+++ binary_search.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"errors"
+)
+
+func binarySearch(haystack []int, needle int) (int, error) {
+	low := 0              // start is inclusive
+	high := len(haystack) // end is exclusive, meaning we will go up to, but not including it
+
+	for low < high {
+		mid := low + (high-low)/2 // wtf.. why floor wont accept int
+		value := haystack[mid]
+
+		if value == needle {
+			return mid, nil
+		} else if value > needle { // then throw out everything to the right
+			high = mid
+		} else if value < needle { // then throw away everything to the left
+			low = mid + 1 // no need to check mid again
+		}
+	}
+
+	return -1, errors.New("needle not found")
+}
---
+++ binary_search_test.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var haystack = []int{1, 3, 4, 69, 71, 81, 90, 99, 420, 1337, 69420}
+
+func TestBinarySearch(t *testing.T) {
+	assert := assert.New(t)
+
+	index, err := binarySearch(haystack, 0)
+	assert.Equal(-1, index)
+	assert.Error(err)
+
+	index, err = binarySearch(haystack, 1)
+	assert.Equal(0, index)
+	assert.Nil(err)
+
+	index, err = binarySearch(haystack, 69)
+	assert.Equal(3, index)
+	assert.Nil(err)
+
+	index, err = binarySearch(haystack, 81)
+	assert.Equal(5, index)
+	assert.Nil(err)
+
+	index, err = binarySearch(haystack, 1336)
+	assert.Equal(-1, index)
+	assert.Error(err)
+
+	index, err = binarySearch(haystack, 69420)
+	assert.Equal(10, index)
+	assert.Nil(err)
+
+	index, err = binarySearch(haystack, 69421)
+	assert.Equal(-1, index)
+	assert.Error(err)
+}
