e3076c6838771a51dc8072da3b60965eeb8194d6
Igor Pavlichenko <igor.o.pavlichenko@gmail.com>
2024-05-15 01:13:21 +0100

    Implement the two crystal balls problem

---
+++ search/two_crystal_balls.go
@@ -0,0 +1,37 @@
+package search
+
+import (
+	"errors"
+	"math"
+)
+
+/*
+Given 2 crystal balls and a 100 floor building. Find out the maximum height from which a ball
+can be dropped without breaking in the most optimized way.
+
+Worst-case complexity: O(√n)
+Best complexity: O(1)
+*/
+func two_crystal_balls(floors []bool) (int, error) {
+	// we want to reach sub-linear complexity, and we can't really reach log here, that's why we use square root
+	jumpAmount := int(math.Floor(math.Sqrt(float64(len(floors)))))
+
+	i := jumpAmount // start at index we jumped to
+	for ; i < len(floors); i += jumpAmount {
+		if floors[i] {
+			break // means our first crystal ball has broken
+		}
+	}
+
+	// so now we "jump" back one jump, and start dropping the crystal ball one floor at a time
+	i -= jumpAmount
+	// we could limit the amount of walking to "jumpAmount"
+	// but we know for sure that it will break before reaching that anyway
+	for ; i < len(floors); i++ {
+		if floors[i] {
+			return i, nil
+		}
+	}
+
+	return -1, errors.New("balls never broke")
+}
---
+++ search/two_crystal_balls_test.go
@@ -0,0 +1,50 @@
+package search
+
+import (
+	"math/rand"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTwoCrystalBalls(t *testing.T) {
+	assert := assert.New(t)
+
+	idx := rand.Intn(10000)
+	floors := make([]bool, 10000)
+
+	for i := idx; i < len(floors); i++ {
+		floors[i] = true
+	}
+
+	index, err := two_crystal_balls(floors)
+	assert.Equal(idx, index)
+	assert.Nil(err)
+}
+
+func TestTwoCrystalBalls_firstFloor(t *testing.T) {
+	assert := assert.New(t)
+
+	floors := []bool{false, true, true, true, true, true, true, true, true}
+	index, err := two_crystal_balls(floors)
+	assert.Equal(1, index)
+	assert.Nil(err)
+}
+
+func TestTwoCrystalBalls_zerothFloor(t *testing.T) {
+	assert := assert.New(t)
+
+	floors := []bool{true, true, true, true, true, true, true, true, true}
+	index, err := two_crystal_balls(floors)
+	assert.Equal(0, index)
+	assert.Nil(err)
+}
+
+func TestTwoCrystalBalls_ballsNeverBreak(t *testing.T) {
+	assert := assert.New(t)
+
+	floors := []bool{false, false, false, false, false, false, false, false, false}
+	index, err := two_crystal_balls(floors)
+	assert.Equal(-1, index)
+	assert.Error(err)
+}
