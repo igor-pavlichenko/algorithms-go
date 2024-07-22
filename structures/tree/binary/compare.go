35fe362955257e6492036c6072f859ffcac5a20d
Igor Pavlichenko <igor.o.pavlichenko@gmail.com>
2024-07-22 22:19:24 +0100

    Add tests for binary tree comparison algorithm

---
+++ structures/tree/binary/compare.go
@@ -0,0 +1,6 @@
+package binarytree
+
+func Compare(a *Node[int], b *Node[int]) bool {
+
+	return false
+}
---
+++ structures/tree/binary/compare_test.go
@@ -0,0 +1,52 @@
+package binarytree
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var tree2 Node[int] = Node[int]{
+	value: 20,
+	right: &Node[int]{
+		value: 50,
+		right: &Node[int]{
+			value: 100,
+			right: nil,
+			left:  nil,
+		},
+		left: &Node[int]{
+			value: 30,
+			right: nil,
+			left: &Node[int]{
+				value: 29,
+				right: nil,
+				left:  nil,
+			},
+		},
+	},
+	left: &Node[int]{
+		value: 10,
+		right: &Node[int]{
+			value: 15,
+			right: nil,
+			left:  nil,
+		},
+		left: &Node[int]{
+			value: 5,
+			right: nil,
+			left:  nil,
+		},
+	},
+}
+
+func TestCompare(t *testing.T) {
+	assert := assert.New(t)
+
+	assert.True(Compare(&tree, &tree))
+	assert.True(Compare(&tree2, &tree2))
+	assert.True(Compare(nil, nil))
+	assert.False(Compare(nil, &tree))
+	assert.False(Compare(&tree, nil))
+	assert.False(Compare(&tree, &tree2))
+}
